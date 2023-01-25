73844e469a261aea1b651e4bd5a14f6e607c593e
Rupak Biswas <rupakbiswas@thoucentric.com>
2023-01-25 19:46:40 +0530

    Net X

--- Network X/Form1.Designer.cs
+++ Network X/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Network_X
 {
-    partial class Form1
+    partial class networkX
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblGateway = new System.Windows.Forms.Label();
+            this.lblDns = new System.Windows.Forms.Label();
+            this.sysGateway = new System.Windows.Forms.Label();
+            this.sysDNSServers = new System.Windows.Forms.Label();
+            this.sysIP = new System.Windows.Forms.Label();
+            this.checkNIC = new System.Windows.Forms.Button();
+            this.netXResult = new System.Windows.Forms.Label();
+            this.networkStatus = new System.Windows.Forms.Button();
+            this.flushDNS = new System.Windows.Forms.Button();
+            this.releaseRenewIP = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(94, 49);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(57, 20);
+            this.lblIP.TabIndex = 0;
+            this.lblIP.Text = "Your IP:";
+            // 
+            // lblGateway
+            // 
+            this.lblGateway.AutoSize = true;
+            this.lblGateway.Location = new System.Drawing.Point(94, 83);
+            this.lblGateway.Name = "lblGateway";
+            this.lblGateway.Size = new System.Drawing.Size(102, 20);
+            this.lblGateway.TabIndex = 1;
+            this.lblGateway.Text = "Your Gateway:";
+            // 
+            // lblDns
+            // 
+            this.lblDns.AutoSize = true;
+            this.lblDns.Location = new System.Drawing.Point(94, 119);
+            this.lblDns.Name = "lblDns";
+            this.lblDns.Size = new System.Drawing.Size(75, 20);
+            this.lblDns.TabIndex = 2;
+            this.lblDns.Text = "Your DNS:";
+            // 
+            // sysGateway
+            // 
+            this.sysGateway.AutoSize = true;
+            this.sysGateway.Location = new System.Drawing.Point(242, 83);
+            this.sysGateway.Name = "sysGateway";
+            this.sysGateway.Size = new System.Drawing.Size(66, 20);
+            this.sysGateway.TabIndex = 3;
+            this.sysGateway.Text = "Gateway";
+            // 
+            // sysDNSServers
+            // 
+            this.sysDNSServers.AutoSize = true;
+            this.sysDNSServers.Location = new System.Drawing.Point(242, 119);
+            this.sysDNSServers.Name = "sysDNSServers";
+            this.sysDNSServers.Size = new System.Drawing.Size(39, 20);
+            this.sysDNSServers.TabIndex = 4;
+            this.sysDNSServers.Text = "DNS";
+            // 
+            // sysIP
+            // 
+            this.sysIP.AutoSize = true;
+            this.sysIP.Location = new System.Drawing.Point(242, 49);
+            this.sysIP.Name = "sysIP";
+            this.sysIP.Size = new System.Drawing.Size(21, 20);
+            this.sysIP.TabIndex = 5;
+            this.sysIP.Text = "IP";
+            // 
+            // checkNIC
+            // 
+            this.checkNIC.Location = new System.Drawing.Point(69, 225);
+            this.checkNIC.Name = "checkNIC";
+            this.checkNIC.Size = new System.Drawing.Size(194, 55);
+            this.checkNIC.TabIndex = 6;
+            this.checkNIC.Text = "Check NIC";
+            this.checkNIC.UseVisualStyleBackColor = true;
+            this.checkNIC.Click += new System.EventHandler(this.checkNIC_Click);
+            // 
+            // netXResult
+            // 
+            this.netXResult.AutoSize = true;
+            this.netXResult.Location = new System.Drawing.Point(94, 174);
+            this.netXResult.Name = "netXResult";
+            this.netXResult.Size = new System.Drawing.Size(49, 20);
+            this.netXResult.TabIndex = 7;
+            this.netXResult.Text = "Status";
+            // 
+            // networkStatus
+            // 
+            this.networkStatus.Location = new System.Drawing.Point(305, 225);
+            this.networkStatus.Name = "networkStatus";
+            this.networkStatus.Size = new System.Drawing.Size(194, 55);
+            this.networkStatus.TabIndex = 8;
+            this.networkStatus.Text = "Network Status";
+            this.networkStatus.UseVisualStyleBackColor = true;
+            this.networkStatus.Click += new System.EventHandler(this.networkStatus_Click);
+            // 
+            // flushDNS
+            // 
+            this.flushDNS.Location = new System.Drawing.Point(545, 225);
+            this.flushDNS.Name = "flushDNS";
+            this.flushDNS.Size = new System.Drawing.Size(194, 55);
+            this.flushDNS.TabIndex = 9;
+            this.flushDNS.Text = "Flush DNS";
+            this.flushDNS.UseVisualStyleBackColor = true;
+            this.flushDNS.Click += new System.EventHandler(this.flushDNS_Click);
+            // 
+            // releaseRenewIP
+            // 
+            this.releaseRenewIP.Location = new System.Drawing.Point(69, 313);
+            this.releaseRenewIP.Name = "releaseRenewIP";
+            this.releaseRenewIP.Size = new System.Drawing.Size(194, 55);
+            this.releaseRenewIP.TabIndex = 10;
+            this.releaseRenewIP.Text = "Release/Renew IP";
+            this.releaseRenewIP.UseVisualStyleBackColor = true;
+            this.releaseRenewIP.Click += new System.EventHandler(this.releaseRenewIP_Click);
+            // 
+            // networkX
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.releaseRenewIP);
+            this.Controls.Add(this.flushDNS);
+            this.Controls.Add(this.networkStatus);
+            this.Controls.Add(this.netXResult);
+            this.Controls.Add(this.checkNIC);
+            this.Controls.Add(this.sysIP);
+            this.Controls.Add(this.sysDNSServers);
+            this.Controls.Add(this.sysGateway);
+            this.Controls.Add(this.lblDns);
+            this.Controls.Add(this.lblGateway);
+            this.Controls.Add(this.lblIP);
+            this.Name = "networkX";
+            this.Text = "Network X";
+            this.Load += new System.EventHandler(this.networkX_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label lblIP;
+        private Label lblGateway;
+        private Label lblDns;
+        private Label sysGateway;
+        private Label sysDNSServers;
+        private Label sysIP;
+        private Button checkNIC;
+        private Label netXResult;
+        private Button networkStatus;
+        private Button flushDNS;
+        private Button releaseRenewIP;
     }
 }
--- Network X/Form1.cs
+++ Network X/Form1.cs
@@ -1,10 +1,175 @@
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
 namespace Network_X
 {
-    public partial class Form1 : Form
+    public partial class networkX : Form
     {
-        public Form1()
+        public networkX()
         {
             InitializeComponent();
+        }
+
+        private void networkX_Load(object sender, EventArgs e)
+        {
+            string ipAddress = GetIPAddress();
+            string defaultGateway = GetDefaultGateway();
+            string dnsServers = GetDNSServers();
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                sysIP.Text = ipAddress;
+            }
+            else
+            {
+                sysIP.Text = "No IP address found.";
+            }
+            if (!string.IsNullOrEmpty(defaultGateway))
+            {
+                sysGateway.Text = defaultGateway;
+            }
+            else
+            {
+                sysGateway.Text = "No default gateway found.";
+            }
+            if (!string.IsNullOrEmpty(dnsServers))
+            {
+                sysDNSServers.Text = dnsServers;
+            }
+            else
+            {
+                sysDNSServers.Text = "No DNS servers found.";
+            }
+        }
+
+        private string GetIPAddress()
+        {
+            var card = NetworkInterface.GetAllNetworkInterfaces()
+            .Where(n => n.OperationalStatus == OperationalStatus.Up)
+            .Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+            .SelectMany(n => n.GetIPProperties()?.UnicastAddresses)
+            .Where(a => a.Address.AddressFamily == AddressFamily.InterNetwork)
+            .FirstOrDefault();
+            return card?.Address.ToString();
+        }
+
+        private string GetDefaultGateway()
+        {
+            var card = NetworkInterface.GetAllNetworkInterfaces()
+            .Where(n => n.OperationalStatus == OperationalStatus.Up)
+            .Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+            .SelectMany(n => n.GetIPProperties()?.GatewayAddresses)
+            .FirstOrDefault(g => g?.Address != null);
+            return card?.Address.ToString();
+        }
+
+        private string GetDNSServers()
+        {
+            var card = NetworkInterface.GetAllNetworkInterfaces()
+            .Where(n => n.OperationalStatus == OperationalStatus.Up)
+            .Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+            .SelectMany(n => n.GetIPProperties()?.DnsAddresses)
+            .FirstOrDefault();
+            return string.Join(", ", card);
+        }
+
+        private void checkNIC_Click(object sender, EventArgs e)
+        {
+            string ipAddress = "127.0.0.1";
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            options.DontFragment = true;
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send(ipAddress, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                netXResult.Text = "Ping to " + ipAddress + " was successful." + " RoundTrip time: " + reply.RoundtripTime + "ms";
+            }
+            else
+            {
+                netXResult.Text = "Ping to " + ipAddress + " failed. Status: " + reply.Status;
+            }
+        }
+
+        private void networkStatus_Click(object sender, EventArgs e)
+        {
+            string hostname = "google.com";
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            options.DontFragment = true;
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send(hostname, timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                netXResult.Text = "Ping to " + hostname + " was successful." + " RoundTrip time: " + reply.RoundtripTime + "ms";
+            }
+            else
+            {
+                netXResult.Text = "Ping to " + hostname + " failed. Status: " + reply.Status;
+            }
+        }
+
+        private void flushDNS_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c ipconfig /flushdns");
+
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                MessageBox.Show("DNS cache flushed.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error flushing DNS cache: " + ex.Message);
+            }
+        }
+
+        private void releaseRenewIP_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo releaseProcStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c ipconfig /release");
+
+                releaseProcStartInfo.RedirectStandardOutput = true;
+                releaseProcStartInfo.UseShellExecute = false;
+                releaseProcStartInfo.CreateNoWindow = true;
+
+                System.Diagnostics.Process releaseProc = new System.Diagnostics.Process();
+                releaseProc.StartInfo = releaseProcStartInfo;
+                releaseProc.Start();
+                string releaseResult = releaseProc.StandardOutput.ReadToEnd();
+
+                System.Diagnostics.ProcessStartInfo renewProcStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c ipconfig /renew");
+
+                renewProcStartInfo.RedirectStandardOutput = true;
+                renewProcStartInfo.UseShellExecute = false;
+                renewProcStartInfo.CreateNoWindow = true;
+
+                System.Diagnostics.Process renewProc = new System.Diagnostics.Process();
+                renewProc.StartInfo = renewProcStartInfo;
+                renewProc.Start();
+                string renewResult = renewProc.StandardOutput.ReadToEnd();
+
+                MessageBox.Show("IP address released and renewed.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error releasing and renewing IP address: " + ex.Message);
+            }
         }
     }
 }
